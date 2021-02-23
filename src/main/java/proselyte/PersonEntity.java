3c5d5577e7c9256eb2672cbc9a6c06d4968cd2cb
pkhshanovskyi <pavlo.khshanovskyi@globallogic.com>
2021-02-23 10:44:34 +0200

    PersonEntityBranch proselyte step1

---
+++ src/main/java/proselyte/PersonEntity.java
@@ -0,0 +1,13 @@
+package proselyte;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class PersonEntity {
+    private String firstName;
+    private String lastName;
+}
