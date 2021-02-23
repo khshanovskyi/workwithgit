a3c094cce3a27cf081b607d498fa864ddf3f90a9
pkhshanovskyi <pavlo.khshanovskyi@globallogic.com>
2021-02-23 10:48:52 +0200

    PersonEntityBranch proselyte step1

---
+++ src/main/java/proselyte/Main.java
@@ -0,0 +1,10 @@
+package proselyte;
+
+public class Main {
+    public static void main(String[] args) {
+        BaseEntity baseEntity = null;
+        if (baseEntity.isNew()){
+            baseEntity = new BaseEntity();
+        }
+    }
+}
